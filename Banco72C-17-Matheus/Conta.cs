0e8829518be7e720f2bf3672ff7672467afe2dd6
Pedro Kayami Franchini Scarpin <pedro.kayami@unesp.br>
2022-05-25 19:26:36 -0300

    Adicionar arquivos de projeto.

---
+++ Banco72C-17-Matheus/Conta.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Banco72C_17_Matheus
+{
+    public class Conta
+    {
+        // Criação dos atributos internos da classe
+        private Int64 numero;
+        private decimal saldo;
+        private decimal limite;
+
+        // Criação das propriedades da classe
+        public Int64 Numero
+        {
+            get { return numero; }
+            set { numero = value; }
+        }
+        public decimal Saldo
+        {
+            get { return saldo; }
+            set { saldo = value; }
+        }
+
+        public Decimal Limite
+        {
+            get { return limite; }
+            set { limite = value; }
+        }
+    }
+}
